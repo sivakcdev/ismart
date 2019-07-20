eab70e898f95c028c2d8fc9e84fb9c90d2bc44be
shiva <shiva@gmail.com>
2019-07-20 11:55:28 +0000

    ismart

---
+++ src/main/java/com/ismart/App.java
@@ -0,0 +1,13 @@
+package com.ismart;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
